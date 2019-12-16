17fab11537e7e9c394546863163c351585c08aba
yelmee <qkfrk94@gmail.com>
2019-12-17 00:41:32 +0900

    Add Login Signup Layout

---
+++ app/src/main/java/com/avon/spott/SignupActivity.kt
@@ -0,0 +1,25 @@
+package com.avon.spott
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.view.View.OnFocusChangeListener
+
+
+class SignupActivity : AppCompatActivity(){
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+}
+
+}
