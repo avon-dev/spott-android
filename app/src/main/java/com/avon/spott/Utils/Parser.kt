fefeead64610d1708f9f92ff2b304b435df751c2
SeungHyunBaek <back947@naver.com>
2020-01-03 23:03:03 +0900

    Modify Parser

--- app/src/main/java/com/avon/spott/Utils/Parser.kt
+++ app/src/main/java/com/avon/spott/Utils/Parser.kt
@@ -6,13 +6,22 @@ import org.json.JSONObject
 
 class Parser<T>(val type: T) {
     companion object {
+        private val TAG = "Parser"
+
         fun <T> toJson(data: T): String {
-            return JSONObject().put("sending", GsonBuilder().create().toJson(data)).toString()
+//            val result = JSONObject().put("sending", GsonBuilder().create().toJson(data)).toString()
+            val result = GsonBuilder().create().toJson(data).toString()
+//            logd(TAG, "toJson(): $result")
+            return result
         }
 
         inline fun <reified T> fromJson(payload: String): T {
-            return GsonBuilder().create()
-                .fromJson(JSONObject(payload).getString("payload"), object : TypeToken<T>() {}.type)
+            // 객체 json을 받을 때
+//            return GsonBuilder().create().fromJson(payload, object : TypeToken<T>() {}.type)
+            // payload 파싱된 문자열로 받을 때
+            return GsonBuilder().create().fromJson(JSONObject(payload).getString("payload"), object : TypeToken<T>() {}.type)
+//             // JSONObject
+//            return GsonBuilder().create().fromJson(payload.getString("payload"), object : TypeToken<T>() {}.type)
         }
     }
 }
