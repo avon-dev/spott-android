48b21e732cf0119f19f25fc82541c20b2359210b
SeungHyunBaek <back947@naver.com>
2020-01-08 10:18:16 +0900

    Modify Retrofit

--- app/src/main/java/com/avon/spott/Utils/Retrofit.kt
+++ app/src/main/java/com/avon/spott/Utils/Retrofit.kt
@@ -1,17 +1,45 @@
 package com.avon.spott.Utils
 
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class NetRetrofit {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("")
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 
-    fun getService(): RetrofitService {
-        return retrofit.create(RetrofitService::class.java)
+class Retrofit(baseUrl:String) {
+    private val retrofitService =
+        createRetrofit(baseUrl).create(RetrofitService::class.java)
+
+    private fun createRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    fun signIn(url:String, email:String, password:String):Observable<Response<String>> {
+        return retrofitService.signIn(url, email, password)
+            .subscribeOn(Schedulers.io())
+            .map { t -> if (t.isSuccessful) t else throw HttpException(t) }
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun get(url: String, sending: String): Observable<Response<String>> {
+        return retrofitService.get(url, sending)
+            .subscribeOn(Schedulers.io())
+            .map { t -> if (t.isSuccessful) t else throw HttpException(t) }
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun post(url: String, sending: String): Observable<Response<String>> {
+        return retrofitService.post(url, sending)
+            .subscribeOn(Schedulers.io())
+            .map { t -> if (t.isSuccessful) t else throw HttpException(t) }
+            .observeOn(AndroidSchedulers.mainThread())
     }
 }
