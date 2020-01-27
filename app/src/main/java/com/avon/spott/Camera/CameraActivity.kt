fedc9026140e69d96409b4c7a189398137f60d2d
SeungHyunBaek <back947@naver.com>
2020-01-27 20:07:33 +0900

    Camera

--- app/src/main/java/com/avon/spott/Camera/CameraActivity.kt
+++ app/src/main/java/com/avon/spott/Camera/CameraActivity.kt
@@ -1,23 +1,17 @@
 package com.avon.spott.Camera
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.avon.spott.CameraFragment
 import com.avon.spott.R
 
-class CameraActivity : AppCompatActivity(), View.OnClickListener {
+class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-
-        savedInstanceState ?: supportFragmentManager.beginTransaction()
-            .replace(R.id.frame_surface_camera_a, CameraFragment.newInstance())
-            .commit()
-    }
-
-    override fun onClick(v: View?) {
-
+//
+//        savedInstanceState ?: supportFragmentManager.beginTransaction()
+//            .replace(R.id.frame_surface_camera_a, CameraFragment.newInstance())
+//            .commit()
     }
 }
