d3392529236bbac9c2b97641cca28bf7c4df2d25
SeungHyunBaek <back947@naver.com>
2020-02-21 19:33:04 +0900

    Add google login api

--- app/src/main/java/com/avon/spott/Login/LoginActivity.kt
+++ app/src/main/java/com/avon/spott/Login/LoginActivity.kt
@@ -13,12 +13,22 @@ import com.avon.spott.EmailLogin.EmailLoginActivity
 import com.avon.spott.Main.MainActivity
 import com.avon.spott.Password.PasswordActivity
 import com.avon.spott.R
+import com.avon.spott.Utils.logd
+import com.avon.spott.Utils.loge
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListener {
 
     private lateinit var loginPresenter: LoginPresenter
     override lateinit var presenter: LoginContract.Presenter
+
+    private val RC_SIGN_IN = 1
+    private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +41,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
     private fun init() {
         loginPresenter = LoginPresenter(this)
 
+        btn_googlelogin_login_a.setOnClickListener(this)
         btn_emaillogin_login_a.setOnClickListener(this)
         text_signup_login_a.setOnClickListener(this)
 
@@ -65,12 +76,42 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
         startActivity(intent)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            logd(TAG, "${account.toString()}")
+            Intent(this@LoginActivity, MainActivity::class.java).let { startActivity(it) }
+        } catch (e:ApiException) {
+            loge(TAG, "signInResult:failed code=${e.statusCode}, ${e.message}")
+            println(e.stackTrace.toString())
+        }
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btn_emaillogin_login_a -> {
+            R.id.btn_googlelogin_login_a -> { // 구글 로그인
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build();
+
+                val googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+                val signInIntent = googleSignInClient.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }
+            R.id.btn_emaillogin_login_a -> { // 이메일 로그인
                 presenter.openEmailLogin()
             }
-            R.id.text_signup_login_a -> {
+            R.id.text_signup_login_a -> { // 회원가입
                 presenter.openSignup()
             }
         }
