d7078cf35400bfff10b3f4a505f73aea35d2d25d
SeungHyunBaek <back947@naver.com>
2020-03-02 14:34:39 +0900

    Fix socialLogin, nickname

--- app/src/main/java/com/avon/spott/Login/LoginActivity.kt
+++ app/src/main/java/com/avon/spott/Login/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.avon.spott.Data.User
 import com.avon.spott.Email.EmailActivity
@@ -16,9 +17,16 @@ import com.avon.spott.EmailLogin.EmailLoginActivity
 import com.avon.spott.Main.MainActivity
 import com.avon.spott.Nickname.NicknameActivity
 import com.avon.spott.Password.PasswordActivity
+import com.avon.spott.R
 import com.avon.spott.Utils.MySharedPreferences
 import com.avon.spott.Utils.logd
 import com.avon.spott.Utils.loge
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -41,8 +49,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
     private val RC_SIGN_IN = 1
     private val TAG = "LoginActivity"
 
-
-//    private lateinit var callbackManager:CallbackManager
+    private lateinit var callbackManager:CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,55 +77,65 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
         btn_googlelogin_login_a.setOnClickListener(this)
         btn_emaillogin_login_a.setOnClickListener(this)
         text_signup_login_a.setOnClickListener(this)
-        btn_facebooklogin_a.setOnClickListener(this)
+//        btn_facebooklogin_a.setOnClickListener(this)
 
-//        callbackManager = CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
 
 
 //        btn_facebooklogin_a.setReadPermissions("email")
-//        btn_facebooklogin_a.setPermissions(Arrays.asList("email"))
+        btn_facebooklogin_a.setPermissions(Arrays.asList("email"))
 
 //        val loginButton = findViewById(R.id.btn_facebooklogin_a) as LoginButton
 
         // Callback registration
-//        btn_facebooklogin_a.registerCallback(callbackManager, object :
-//            FacebookCallback<LoginResult?> {
-//            override fun onSuccess(loginResult: LoginResult?) { // App code
+        btn_facebooklogin_a.registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) { // App code
+
+                if(loginResult != null) {
+                    logd(TAG, "success: ${loginResult.accessToken}")
+
+                    val request = GraphRequest.newMeRequest(loginResult.accessToken, GraphRequest.GraphJSONObjectCallback { jsonObject, response ->
+                        try {
+                            if(jsonObject.has("email")) { // 이메일을 허락해줬을 때
+                                logd(TAG, "fb email: " + jsonObject.getString("email"))
+                                Toast.makeText(this@LoginActivity, "페북 로그인 성공", Toast.LENGTH_SHORT).show()
+
+                                Intent(applicationContext, NicknameActivity::class.java).let {
+                                    it.putExtra("login", 1)
+//                                    val socialUser = SocialUser(jsonObject.getString("email"))
+                                    it.putExtra("email", jsonObject.getString("email"))
+//                                    it.putExtra("email", jsonObject.getString("email"))
+                                    startActivity(it)
+                                }
+
+                            } else { // 이메일을 허락하지 않았을 때
+                                // 로그인 실패로 간주하고 권한을 허락해달라고 해야 함.
+                                LoginManager.getInstance().logOut()
+//                                btn_facebooklogin_a.simulateClick()
+                                Toast.makeText(this@LoginActivity, "페북 로그인 실패 (권한이 필요합니다)", Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e:Exception) {
+                            e.printStackTrace()
+                        }
+                    })
 //
-//                if(loginResult != null) {
-//                    logd(TAG, "success: ${loginResult.accessToken}")
-//
-//                    val request = GraphRequest.newMeRequest(loginResult.accessToken, GraphRequest.GraphJSONObjectCallback { jsonObject, response ->
-//                        try {
-//                            if(jsonObject.has("email")) { // 이메일을 허락해줬을 때
-//                                logd(TAG, "fb email: " + jsonObject.getString("email"))
-//                                Toast.makeText(this@LoginActivity, "페북 로그인 성공", Toast.LENGTH_SHORT).show()
-//                            } else { // 이메일을 허락하지 않았을 때
-//                                // 로그인 실패로 간주하고 권한을 허락해달라고 해야 함.
-//                                LoginManager.getInstance().logOut()
-////                                btn_facebooklogin_a.simulateClick()
-//                                Toast.makeText(this@LoginActivity, "페북 로그인 실패 (권한이 필요합니다)", Toast.LENGTH_SHORT).show()
-//                            }
-//                        } catch (e:Exception) {
-//                            e.printStackTrace()
-//                        }
-//                    })
-//
-//                    val parameter = Bundle()
-//                    parameter.putString("fields", "id,name,email");
-//                    request.parameters = parameter
-//                    request.executeAsync()
-//                }
-//            }
-//
-//            override fun onCancel() { // App code
-//            }
-//
-//            override fun onError(exception: FacebookException) { // App code
-//                loge(TAG, "${exception.message}")
-//                exception.printStackTrace()
-//            }
-//        })
+                    val parameter = Bundle()
+                    parameter.putString("fields", "id,name,email");
+                    request.parameters = parameter
+                    request.executeAsync()
+                }
+            }
+
+            override fun onCancel() { // App code
+                logd(TAG, "fb cancel")
+            }
+
+            override fun onError(exception: FacebookException) { // App code
+                loge(TAG, "${exception.message}")
+                exception.printStackTrace()
+            }
+        })
 
 
 
@@ -212,7 +229,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        callbackManager.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
@@ -246,7 +263,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            com.avon.spott.R.id.btn_googlelogin_login_a -> { // 구글 로그인
+            R.id.btn_googlelogin_login_a -> { // 구글 로그인
                 val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestEmail()
                     .build();
@@ -256,10 +273,10 @@ class LoginActivity : AppCompatActivity(), LoginContract.View, View.OnClickListe
                 val signInIntent = googleSignInClient.signInIntent
                 startActivityForResult(signInIntent, RC_SIGN_IN)
             }
-            com.avon.spott.R.id.btn_emaillogin_login_a -> { // 이메일 로그인
+            R.id.btn_emaillogin_login_a -> { // 이메일 로그인
                 presenter.openEmailLogin()
             }
-            com.avon.spott.R.id.text_signup_login_a -> { // 회원가입
+            R.id.text_signup_login_a -> { // 회원가입
                 presenter.openSignup()
             }
 
