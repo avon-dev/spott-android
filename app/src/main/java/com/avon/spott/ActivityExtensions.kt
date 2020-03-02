ec8bddfd221afe2cd085b335becfe1a2b0171224
SeungHyunBaek <back947@naver.com>
2020-03-02 15:12:44 +0900

    Add showToast

--- app/src/main/java/com/avon/spott/ActivityExtensions.kt
+++ app/src/main/java/com/avon/spott/ActivityExtensions.kt
@@ -2,7 +2,12 @@ package com.avon.spott
 
 import android.widget.Toast
 import com.avon.spott.Camera.CameraActivity
+import com.avon.spott.Camera.CameraXActivity
 
 fun CameraActivity.showToast(text:String) {
+    runOnUiThread { Toast.makeText(this, text, Toast.LENGTH_SHORT).show() }
+}
+
+fun CameraXActivity.showToast(text:String) {
     runOnUiThread { Toast.makeText(this, text, Toast.LENGTH_SHORT).show() }
 }
